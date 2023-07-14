41a9c1d65d0690676091950f7c3ec66e216c6a85
blackgenius <zohoungbogboanlyou@gmail.com>
2023-07-14 01:48:19 +0100

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print something
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
