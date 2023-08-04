b5e20c43c7f59f034fe586408fb7876be2aec8fa
blackgenius <zohoungbogboanlyou@gmail.com>
2023-08-04 19:01:04 +0100

    check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * string_len - returns the length of a string
+ * @s: string to get length of
+ * Return: length of string
+ */
+
+int string_len(char *s)
+{
+	if (s == NULL && *s == '\0')
+		return (0);
+
+	return (1 + string_len(s + 1));
+}
+
+/**
+ * is_palindrome_helper - checks if a string is a palindrome
+ * @s: string to check
+ * @position: current position in string
+ * @string_len: length of string
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+
+int is_palindrome_helper(char *s, int position, int string_len)
+{
+	if (position >= string_len)
+		return (1);
+
+	if (s[position] != s[string_len - 1])
+		return (0);
+
+	return (is_palindrome_helper(s, position + 1, string_len - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	return (is_palindrome_helper(s, 0, string_len(s)));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef __MAIN__H__
 #define __MAIN__H__
 
+#include <unistd.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
