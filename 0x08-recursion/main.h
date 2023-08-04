47ce69a854e736c90efddd31ad2c4938f0e04b4d
blackgenius <zohoungbogboanlyou@gmail.com>
2023-08-04 18:28:45 +0100

    check first task

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN__H__
+#define __MAIN__H__
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int _is_prime_number(int);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
